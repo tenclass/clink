6512046f488faa77c8b859c150d7b97acb76788c
cair <rui.cai@tenclass.com>
2023-04-27 14:28:10 +0800

    add comment header

--- server/utilities/logger.cc
+++ server/utilities/logger.cc
@@ -1,6 +1,6 @@
 /* 
  * CCLink
- * Copyright (C) 2021 cair <rui.cai@tenclass.com>
+ * Copyright (C) 2023 cair <rui.cai@tenclass.com>
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
